9c42ea2e8ee63902370ffd7ce4976c2eb5f241a1
tathomartinez <tathomartinez@gmail.com>
2022-04-09 19:53:16 -0500

    El commit inicial de login

---
+++ app/src/main/java/com/cownrice/register/auth/ui/activity/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.cownrice.register.auth.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cownrice.register.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
---
+++ app/src/main/java/com/cownrice/register/home/ui/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.cownrice.register.home.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cownrice.register.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
